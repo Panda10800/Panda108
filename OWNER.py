c8514dd292d2db28b59a2cbae0b192badf4316d8
Panda10800 <wfonwfojwsjfoacmecocja@gmail.com>
2024-08-05 19:55:50 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["L_Y_Y_Y", "A_A_5"]
+OWNER_NAME = "KERO DEV"
+BOT_TOKEN = "7343306929:AAErf-lwN3r4jDPx0lisJYDHThiDKDh65DI"
+DATABASE = "mongodb+srv://vv1vbvg:tahLdAddP2xj6R5b@cluster0.4xpikbs.mongodb.net"
+CHANNEL = "https://t.me/Z_2_X_7"
+GROUP = "https://t.me/QD_JZ"
+VIDEO = "https://graph.org/file/bbe526c61648eebca422c.jpg"
+LOGS = "L_Y_Y_Y"
+
